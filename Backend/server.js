5fc766a0067d325dbbf4ab9fa9f7126adfddb763
MathFerreira01 <ferreiramatheus124@gmail.com>
2023-01-12 20:47:03 -0300

    feat: adicionando o orm prisma e typescript

--- Backend/server.js
+++
@@ -1,7 +0,0 @@
-import App from "./src/App.js";
-
-const port = process.env.PORT || 3000;
-
-App.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
--- Backend/src/App.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import routes from "./routes/index.js";
-
-const App = express();
-
-App.use(express.json());
-routes(App);
-
-export default App;
